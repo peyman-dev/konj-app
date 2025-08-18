24ca20fdb13416ba3c99307b55d8e8a35d2bc9d4
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-18 11:20:11 +0330

    Initialized the Windows Search bar

--- app/components/common/header/components/search/index.tsx
+++ app/components/common/header/components/search/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Input } from "@mui/joy";
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="w-[785px] h-[48px] border rounded-[8px] overflow-hidden border-[#C3C3C3]">
+      <Input 
+        className="h-full! border-none! ring-0! outline-0! focus:!ring-0 focus:outline-none! focus-within:border-none! font-YekanBakh-Regular!"
+      />
+    </div>
+  );
+};
 
-export default Search
+export default Search;
