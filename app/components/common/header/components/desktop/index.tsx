1a5dd4f3ba43076d08669b31316592dd1b911e4e
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-18 11:38:00 +0330

    Added navbar items to the component

--- app/components/common/header/components/desktop/index.tsx
+++ app/components/common/header/components/desktop/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Logo from "../logo";
+import Search from "../search";
+import BasketDropdown from "../basket";
+import MyLibrary from "../my-library";
+import ProfileDropdown from "../profile-dropdown";
 
 const Desktop = () => {
   return (
-    <nav>Desktop</nav>
-  )
-}
+    <nav className="py-[18px]">  
+      <div className="w-full container flex-between">
+        <Logo />
+        <Search />
+        <div className="**:h-12! flex items-center gap-4 rounded-lg!">
+          <MyLibrary />
+          <BasketDropdown />
+          <ProfileDropdown />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Desktop
+export default Desktop;
