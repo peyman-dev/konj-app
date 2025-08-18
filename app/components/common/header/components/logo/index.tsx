37622fb5cfff71ed265b9cc6459a56dd8f6bdb78
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-18 11:38:24 +0330

    Desktop Logo Component has been developed

--- app/components/common/header/components/logo/index.tsx
+++ app/components/common/header/components/logo/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import LogoSvg from "@/svg/logo.svg";
 
 const Logo = () => {
   return (
-    <div>Logo</div>
-  )
-}
+    <div>
+      <Link href={"/"}>
+        <Image src={LogoSvg.src} width={88} height={50} alt="کنج" />
+      </Link>
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
