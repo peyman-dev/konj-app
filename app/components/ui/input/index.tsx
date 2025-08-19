848da527d12d03d19f81ee919d4ecfe5ef27b179
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-19 10:43:03 +0330

    Created an Ui Component: Input

--- app/components/ui/input/index.tsx
+++ app/components/ui/input/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Input as HeroInput, InputProps } from "@heroui/input";
 
-const Input = () => {
+const Input = ({ ...rest }: InputProps) => {
   return (
-    <div>Input</div>
-  )
-}
+    <React.Fragment>
+      <HeroInput
+        className="w-full! space-y-1!"
+        classNames={{
+          input:
+            "h-10! px-3! font-YekanBakh-Regular! mt-1! focus-within:ring-0! !rounded-none focus-within:outline-gray-100/30",
+          label: "text-xs! mb-5! font-YekanBakh-Regular!",
+        }}
+        variant="flat"
+        color="danger"
+        {...rest}
+      />
+    </React.Fragment>
+  );
+};
 
-export default Input
+export default Input;
