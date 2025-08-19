8cbe5d96f326d2855390425e667a6e8caa6ea567
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-19 12:02:57 +0330

    JWT Sign Configuration added to server/token.ts

--- server/authentication/token.ts
+++ server/authentication/token.ts
@@ -3,10 +3,14 @@ import { configs, TOKEN_EXPIRE_TIME } from "../settings";
 
 export const createToken = async (key: any) => {
   try {
-    const s = jwt.sign(
+    const token = await jwt.sign(
       key,
       process.env.NEXT_PUBLIC_NEXT_PUBLIC_SECRET_KEY as string,
       configs.jwt.sign
     );
-  } catch (error) {}
+
+    return token;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 };
