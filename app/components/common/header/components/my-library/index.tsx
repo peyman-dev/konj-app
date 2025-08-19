6e884d8ada761c1ec6a8a1b9605555fe7a8beb2a
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-19 12:14:23 +0330

    developed header/my-library ui

--- app/components/common/header/components/my-library/index.tsx
+++ app/components/common/header/components/my-library/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import LibrarySvg from "@/svg/library.svg";
 
 const MyLibrary = () => {
   return (
-    <div>MyLibrary</div>
-  )
-}
+    <div className="text-primary-700 cursor-pointer border px-3 rounded-[8px] min-w-max border-[#C3C3C3]! duration-150 active:bg-[#744D7E]/20! flex-center gap-2 h-12">
+      <div>
+      <Image src={LibrarySvg.src} width={20} height={20} alt="کتابخانه" />
+      </div>
+        کتابخانه من
+    </div>
+  );
+};
 
-export default MyLibrary
+export default MyLibrary;
