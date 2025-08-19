e77b1d528dc88c220a01ecdf0f8debea728a5f62
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-19 11:07:43 +0330

    Actions file initialized for Server Actions

---
+++ app/actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { connectToServer } from "@/server/connect";
+
+export const loginAction = async (data: any) => {
+  try {
+    await connectToServer();
+
+    
+  } catch (error) {}
+};
