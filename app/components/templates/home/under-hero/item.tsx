cf9eecf0791bd7dbcba34d79dc0472ef27aba86c
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-20 11:11:55 +0330

    Added Loading for link clicks

--- app/components/templates/home/under-hero/item.tsx
+++ app/components/templates/home/under-hero/item.tsx
@@ -1,18 +1,36 @@
+"use client";
+import useRedirector from "@/app/core/hooks/use-redirector";
 import { UnderHeroItemType } from "@/app/core/lib/types";
+import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useId } from "react";
 
 const UnderHeroItem = ({ title, href, src }: UnderHeroItemType) => {
+  const [isLoading, redirect] = useRedirector();
+  const id = useId();
+
   return (
-    <Link href={href}>
-      <article className="h-[60px] rounded-lg border border-dashed flex-center gap-2">
-        <Image src={src} alt={title} width={36} height={36} />
-        <div>
-          <h5 className="text-xl">{title}</h5>
-        </div>
-      </article>
-    </Link>
+    <article
+      onClick={() => redirect(href)}
+      className="h-[60px] cursor-pointer rounded-lg border border-dashed flex-center gap-2"
+    >
+      {isLoading ? (
+        <span className="text-sm font-YekanBakh-SemiBold flex-center gap-2">
+            <Loader2 className="text-primary-700 size-4 animate-spin duration-150"/>
+            <span>
+                درحال انتقال ...
+            </span>
+        </span>
+      ) : (
+        <React.Fragment>
+          <Image src={src} alt={title} width={36} height={36} />
+          <div>
+            <h5 className="text-xl">{title}</h5>
+          </div>
+        </React.Fragment>
+      )}
+    </article>
   );
 };
 
