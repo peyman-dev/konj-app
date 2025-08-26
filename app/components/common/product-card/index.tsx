a678ba6237e59b4b9c84fdee456a89b6f11932f4
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-26 11:35:22 +0330

    ProductCard.Footer added

--- app/components/common/product-card/index.tsx
+++ app/components/common/product-card/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ProductHeader from "./components/header";
 import Details from "./components/details";
+import Footer from "./components/footer";
 
 const ProductCard = () => {
   return (
-    <article className="min-w-[197px]! bg-white">
-      <ProductHeader
-        src={"/images/book.png"}
-        alt="تصویر محصول"
-      />
+    <article className="min-w-[197px]! rounded-sm overflow-hidden bg-white">
+      <ProductHeader src={"/images/book.png"} alt="تصویر محصول" />
       <Details />
+      <Footer />
     </article>
   );
 };
