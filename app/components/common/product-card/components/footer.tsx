3ff13b29662c911b6a250e68538b037b0ead87bb
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-26 11:36:18 +0330

    ProductCard.Footer has been developed

---
+++ app/components/common/product-card/components/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t p-4 border-zinc-100 flex-between text-sm">
+      <div className="flex line-through items-center">
+        <span className="text-gray-50 font-Farhang-Medium">256,000</span>
+        <p className="text-black font-YekanBakh-Bold">تومان</p>
+      </div>
+      <div className="flex items-center">
+        <span className="text-gray-50 font-Farhang-Medium">256,000</span>
+        <p className="text-black font-YekanBakh-Bold">تومان</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
