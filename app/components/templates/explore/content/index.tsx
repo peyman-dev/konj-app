bee8f1f2352399beaed9ab46bb3586aa3b9447af
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-30 12:11:55 +0330

    Developing Explore page

---
+++ app/components/templates/explore/content/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <main className='grow'>Content</main>
+  )
+}
+
+export default Content
---
+++ app/components/templates/explore/sidebar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <aside className='w-[309px] *:p-4 *:bg-white *:grow'>Sidebar</aside>
+  )
+}
+
+export default Sidebar
