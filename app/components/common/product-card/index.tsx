5e77bd8d2b396cf037641cc1c7c056c290e531f8
Peyman Ahmadi <peymangeek@gmail.com>
2025-08-31 09:38:44 +0330

    Added an Redirector to for Product Links

--- app/components/common/product-card/index.tsx
+++ app/components/common/product-card/index.tsx
@@ -6,18 +6,24 @@ import Footer from "./components/footer";
 import Link from "next/link";
 import useRedirector from "@/app/core/hooks/use-redirector";
 import Redirector from "./components/redirector";
+import Redirecting from "../../ui/loadings/redirecting";
 
 const ProductCard = () => {
   const [isLoading, redirect] = useRedirector();
 
-
   return (
-    <article onClick={() => redirect("/explore/book/example-book")} className="min-w-[197px]! relative cursor-pointer rounded-sm overflow-hidden bg-white">
-      <Redirector isLoading={isLoading}/>
-      <ProductHeader src={"/images/book.png"} alt="تصویر محصول" />
-      <Details />
-      <Footer />
-    </article>
+    <>
+      <article
+        onClick={() => redirect("/explore/book/example-book")}
+        className="min-w-[197px]! relative cursor-pointer rounded-sm overflow-hidden bg-white"
+      >
+        {/* <Redirector isLoading={isLoading}/> */}
+        <ProductHeader src={"/images/book.png"} alt="تصویر محصول" />
+        <Details />
+        <Footer />
+      </article>
+      {isLoading && <Redirecting />}
+    </>
   );
 };
 
