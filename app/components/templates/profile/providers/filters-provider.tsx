f0271475aacdbf4daad40a7fd8be51a5bf892e79
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-08 09:28:27 +0330

    Added some providers for Profile Page

---
+++ app/components/templates/profile/providers/filters-provider.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React, { ReactNode } from "react";
+
+const WithFilteringSystem = ({ children }: { children: ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default WithFilteringSystem;
