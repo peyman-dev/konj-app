be3fb847299cddbf3cd65f9732a376f00d66fec3
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-08 09:28:55 +0330

    Initialized the Basic Infos

---
+++ app/components/templates/profile/pages/basic-infos/elements/row.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Row = ({ label, value }: { label: string; value?: string }) => {
+  return <div className="flex-between">
+    <div>
+        <p></p>
+        <p></p>
+    </div>
+    <button className="text-primary-700 cursor-pointer">
+
+    </button>
+  </div>;
+};
+
+export default Row;
---
+++ app/components/templates/profile/pages/basic-infos/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BasicInformations = () => {
+  return (
+    <section className='px-8 py-10.5 bg-white rounded-[8px] grow'>BasicInformations</section>
+  )
+}
+
+export default BasicInformations
